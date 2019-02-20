1cf4b2446310d7ab6436144a273d521d0fb80084
Petru <hagepetru@gmail.com>
2019-02-20 20:51:17 +0200

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  state = {
+    searchTerm: ''
+  }
+  onTextChange = (event) => {
+    const value = event.target.value; 
+
+     this.setState({searchTerm: value})
+  }
+
+  onSearchClick = () => {
+    console.log(this.state.searchTerm)
+    this.setState({searchTerm: ''})
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Photos
+          </Typography>
+              <TextField 
+              className='textfield'
+              placeholder='Search on Spotify'
+              onChange= {this.onTextChange}
+              value= {this.state.searchTerm} />
+              <Button 
+              onClick={this.onSearchClick}
+              disabled={this.state.searchTerm.length === 0}>Search</Button>
+            </Toolbar>
+          </AppBar>
         </header>
+        <div>
+
+        </div>
       </div>
     );
   }
